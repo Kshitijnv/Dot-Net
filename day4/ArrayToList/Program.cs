5eb17a114d8b9fc3bcc828cdee261b005009a433
dac <dac@students.acts.in>
2023-12-08 21:38:48 +0530

    Collection

---
+++ day4/ArrayToList/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+
+namespace ArrayToList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reading the size of array from User
+            Console.WriteLine("Enter the size of array: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] MyArray = new int[size];
+            //Reading the elements of array from user
+            for (int i=0;i<MyArray.Length;i++)
+            {
+                Console.Write($"Enter {i+1} element : ");
+                MyArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            //Displaying the array
+            Console.WriteLine("Original Array");
+            Program.PrintList(MyArray);
+
+
+            //Converting the FIXED array to ArrayList
+            List<int> MyArrayList = new List<int>(MyArray);
+            //Displaying the arrayList
+            Console.WriteLine(" ArrayList");
+            Program.PrintList(MyArrayList);
+
+
+            //Again Converting the ArrayList to Fixed Array
+            int[] newArray = MyArrayList.ToArray();
+            //Displaying the array
+            Console.WriteLine("Converted Array");
+            Program.PrintList(newArray);
+        }
+        public static void PrintList<T>(IList<T> list)
+        {
+            foreach (var i in list)
+            {
+                Console.WriteLine($" element is {i} ");
+            }
+        }
+    
+
+    }
+}
---
+++ day4/CollectionDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Diagnostics.Metrics;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+
+namespace CollectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, BankAccount> list = new SortedList<int, BankAccount>();
+            Boolean exit = false;
+            while (!exit) { 
+                try
+                {
+                    Console.WriteLine("Enter the choice:"
+                            + "\n1)Open Account \n2)Get Account Sumamry \n3)Withdraw \n4)Deposit");
+                    switch (Convert.ToInt32(Console.ReadLine()))
+                    {
+                        case 1:
+                            Console.WriteLine("Enter your name, account type, ");
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                            case 5:
+                            exit = true;
+                            break;
+                    }
+                    
+                }catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+    public enum AccountType
+    {
+        SAVING,CURRENT
+    }
+
+    internal class BankAccount
+    {
+        //property1: Bank account number
+        public int AccountNum { get; set; }
+        //property2: Name
+        public string Name { get; set; }
+
+        //property3: Account type
+        public AccountType Type { get; set; }
+
+        //property4: Balance
+        public decimal Balance { get; set; }
+        private static int idGenerator = 0;
+        public BankAccount(string name,AccountType type,decimal bal)
+        {
+            this.AccountNum = ++idGenerator;
+            this.Name = name;
+            this.Type = type;
+            this.Balance = bal;
+        } 
+    }
+}
